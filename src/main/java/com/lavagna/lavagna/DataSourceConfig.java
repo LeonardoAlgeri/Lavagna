51fe4a265ae81d9166b013c34ebc490ae46bcc3f
leonardo.algeri <leonardo.algeri@staff.aruba.it>
2020-02-10 17:09:53 +0100

    Cambio DataSource da application.properties a DataSourceConfig

---
+++ src/main/java/com/lavagna/lavagna/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package com.lavagna.lavagna;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.*;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mysql://db4free.net:3306/lavagna?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        dataSourceBuilder.username("lavagna");
+        dataSourceBuilder.password("pinguino01");
+        return dataSourceBuilder.build();
+    }
+}
