d3ce6529579dbe939faaa1f06439b92b3aad003a
leonardo.algeri <leonardo.algeri@staff.aruba.it>
2020-02-10 17:24:41 +0100

    User hidden

--- src/main/java/com/lavagna/lavagna/DataSourceConfig.java
+++ src/main/java/com/lavagna/lavagna/DataSourceConfig.java
@@ -13,7 +13,7 @@ public class DataSourceConfig {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
         dataSourceBuilder.url("jdbc:mysql://db4free.net:3306/lavagna?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-        dataSourceBuilder.username("lavagna");
+        dataSourceBuilder.username(System.getenv("SQL_USER"));
         dataSourceBuilder.password("pinguino01");
         return dataSourceBuilder.build();
     }
